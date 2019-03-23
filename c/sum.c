5c47ea9577115e02283b1dbe5fcaeda72ba37c07
Uliana Ustinova <1v.th1nk3r@gmail.com>
2019-03-23 09:16:05 +0200

    added sum.js & sum.c

---
+++ c/sum.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+int sum(int a, int b) {
+    return a+b;
+}
+
+int main() {
+    int total = sum(10, 4);
+    printf("total %d", total);
+}
---
+++ js/sum.js
@@ -0,0 +1,6 @@
+let total = sum(10, 4);
+console.info('total is: ' + total);
+
+function sum(a,b) {
+    return a+b;
+}
